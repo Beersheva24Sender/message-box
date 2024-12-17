63d053e0f5ef35a9590a8ab9a6347d86b4dae161
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2024-12-17 08:22:23 +0200

    start message box

---
+++ src/main/java/telran/producer/consumer/MessageBox.java
@@ -0,0 +1,9 @@
+package telran.producer.consumer;
+
+public interface MessageBox {
+    void put(String message) throws InterruptedException;
+
+    String take() throws InterruptedException;
+
+    String poll();
+}
